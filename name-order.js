790563e4d49a39dd3e6cf759d6ba4c85942af3c5
NureAlam68 <nurealam151068@gmail.com>
2024-09-14 11:57:30 +0600

    name order by alphabetically

---
+++ name-order.js
@@ -0,0 +1,9 @@
+// 2.  How can you order names of students in your class sequentially?
+
+
+function orderOfName(names) {
+    let name = names.sort();
+    return name;
+}
+let names = ['Sohag', 'Nure Alam', 'Nihal', 'Jui','Dapne'];
+console.log(orderOfName(names));
